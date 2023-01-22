1bcd54e92006d4cdb1b86d05c01d818b0787a59a
Anuj Sharma <69591862+theanujdev@users.noreply.github.com>
2023-01-22 21:14:43 +0530

    array: add contains duplicate problem

---
+++ array_and_hashing/contains_duplicate.py
@@ -0,0 +1,22 @@
+# Contains Duplicate
+# Link - https://leetcode.com/problems/contains-duplicate/
+
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+# Example 1:
+# Input: nums = [1, 2, 3, 1]
+# Output: true
+
+
+class Solution:
+    def containsDuplicate(self, nums) -> bool:
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+        return False
+
+
+nums = [1, 2, 3, 1]
+print(Solution().containsDuplicate(nums))
