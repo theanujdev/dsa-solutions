bd1355a47f8efe503f443ff006f88747cc775c48
Anuj Sharma <69591862+theanujdev@users.noreply.github.com>
2023-01-30 20:50:03 +0530

    linked list: add Reorder List

---
+++ linked-list/reorder_list.py
@@ -0,0 +1,47 @@
+# Reorder List (Medium)
+# Link - https://leetcode.com/problems/reorder-list/
+
+# You are given the head of a singly linked-list. The list can be represented as:
+
+# L0 → L1 → … → Ln - 1 → Ln
+# Reorder the list to be on the following form:
+
+# L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+# You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+
+# Example 1:
+# Input: head = [1, 2, 3, 4]
+# Output: [1, 4, 2, 3]
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head) -> None:
+        # find middle
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
+        # merge two halfs
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
