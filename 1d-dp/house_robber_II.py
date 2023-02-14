da25377757c3299c64777824bd4f2ab1d95ea24b
Anuj Sharma <69591862+theanujdev@users.noreply.github.com>
2023-02-14 23:32:23 +0530

    1d-dp: add House Robber II

---
+++ 1d-dp/house_robber_II.py
@@ -0,0 +1,27 @@
+# House Robber II (Medium)
+# Link - https://leetcode.com/problems/house-robber-ii/
+
+# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+# Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+# Example 1:
+# Input: nums = [2, 3, 2]
+# Output: 3
+# Explanation: You cannot rob house 1 (money=2) and then rob house 3 (money=2), because they are adjacent houses.
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums)
+
+        def helper(nums):
+            rob1, rob2 = 0, 0
+            for n in nums:
+                new_rob = max(n+rob1, rob2)
+                rob1 = rob2
+                rob2 = new_rob
+            return rob2
+
+        return max(helper(nums[1:]), helper(nums[:-1]))
