3343aeed1a32082d6e40960ff168dc8fdb23c3e3
Anuj Sharma <69591862+theanujdev@users.noreply.github.com>
2023-02-27 00:17:12 +0530

    binary: add Single Number

---
+++ binary/single-number.py
@@ -0,0 +1,18 @@
+# Single Number (Easy)
+# Link - https://leetcode.com/problems/single-number/
+
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+# Example 1:
+# Input: nums = [2, 2, 1]
+# Output: 1
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
