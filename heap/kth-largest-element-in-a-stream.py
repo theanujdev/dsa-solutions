c08d8978a30d4d87f37978345e6f465e6aee2675
Anuj Sharma <69591862+theanujdev@users.noreply.github.com>
2023-03-02 22:22:11 +0530

    heap: add Kth Largest Element in a Stream

---
+++ heap/kth-largest-element-in-a-stream.py
@@ -0,0 +1,47 @@
+# Kth Largest Element in a Stream (Easy)
+# Link - https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+# Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+# Implement KthLargest class:
+
+# - KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+# - int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+
+# Example 1:
+# Input
+# ["KthLargest", "add", "add", "add", "add", "add"]
+# [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+# Output
+# [null, 4, 5, 5, 8, 8]
+
+# Explanation
+# KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+# kthLargest.add(3);   // return 4
+# kthLargest.add(5);   // return 5
+# kthLargest.add(10);  // return 5
+# kthLargest.add(9);   // return 8
+# kthLargest.add(4);   // return 8
+
+
+import heapq
+
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
